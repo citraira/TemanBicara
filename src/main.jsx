3094138cf8e0e1628b202e48cbb27ea91492354c
CitraSulpiani <citraira32712@gmail.com>
2026-08-13 00:47:45 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+// Import pendaftar Service Worker otomatis dari plugin PWA
+import { registerSW } from "virtual:pwa-register";
+
+// Daftarkan service worker dengan auto update
+registerSW({ immediate: true });
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
